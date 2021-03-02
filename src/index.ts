1a77f4cb038214a00e6353df2de15e37dbe4a3e1
John Weland <jweland@fluentstream.com>
2021-03-02 11:46:28 -0600

    scoped out the classes and added readme/license files

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import Chain from './chain';
+import Wallet from './wallet';
+
+
+const user1 = new Wallet();
+const user2 = new Wallet();
+const user3 = new Wallet();
+
+user1.sendMoney(50, user2.publicKey);
+user2.sendMoney(23, user3.publicKey);
+user3.sendMoney(5, user2.publicKey)
+
+console.log(Chain.instance)
+console.log(user1)
